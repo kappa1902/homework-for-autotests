ab117caeac8e05cb191619f81a8e41db8443c4e7
Kappa19 <gggh38244@gmail.com>
2026-07-23 21:53:03 +0300

    updated code with comments

--- test_echo.py
+++ test_echo.py
@@ -2,6 +2,8 @@ import requests
 
 BASE_URL = "https://postman-echo.com"
 
+
+GET-запрос: проверка передачи параметров в URL
 def test_get_query_params():
     url = f"{BASE_URL}/get"
     params = {"name":"Andrey", "course": "QA"}
@@ -11,7 +13,7 @@ def test_get_query_params():
     assert response.status_code == 200
     assert response.json()["args"] == params
 
-
+# POST-запрос: проверка отправки обычного текста в теле запроса
 def test_post_raw_text():
     url = f"{BASE_URL}/post"
     text_data = ("Что вершит судьбу человечества? Некое незримое существо или закон? … "
@@ -22,7 +24,7 @@ def test_post_raw_text():
     assert response.status_code == 200
     assert response.json()["data"] == text_data
 
-
+# POST-запрос: проверка отправки данных в формате JSON
 def test_post_json():
     url = f"{BASE_URL}/post"
     payload = {"name": "Andrey", "course": "QA"}
@@ -32,7 +34,7 @@ def test_post_json():
     assert response.status_code == 200
     assert response.json()["json"] == payload
 
-
+# PUT-запрос: проверка обновления данных текстом в кодировке UTF-8
 def test_put_raw_text():
     url = f"{BASE_URL}/put"
     payload = ("""В этом мире нет ни правды, ни лжи. Есть только факты.
@@ -40,10 +42,11 @@ def test_put_raw_text():
                Другого способа жить мы не знаем. А те, кому не нравится, что это факты — принимают их за истину.
                Так мы и входим в заблуждение.""")
     response = requests.put(url, data=payload.encode('utf-8'))
+
     assert response.status_code == 200
     assert response.json()["data"] == payload
 
-
+# GET-запрос: проверка передачи пользовательских заголовков (headers)
 def test_custom_headers():
     url = f"{BASE_URL}/get"
     headers = {"Custom-Header": "QA-Auto-Test"}
